93bc29fb793a85239d476080fdddf172985df773
Asif Foysal <asiffoysal104@gmail.com>
2026-01-13 20:44:58 +0600

    first commit

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./configs/db.config.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+/* Middleware */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+/* Test Route */
+app.get("/", (req, res) => {
+  res.send("🚀 Server is running successfully!");
+});
+
+/* DB */
+connectDB();
+
+/* Server */
+app.listen(PORT, () => {
+  console.log(`🔥 Server running on port ${PORT}`);
+});
