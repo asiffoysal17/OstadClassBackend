99e10cb5bfe0f5be8ebb417d923ce5650a196033
Asif Foysal <asiffoysal104@gmail.com>
2026-01-23 01:02:42 +0600

    try

--- src/server.js
+++ src/server.js
@@ -1,34 +1,48 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
 import cookieParser from "cookie-parser";
-import connectDB from "./configs/db.config.js";
-import userRoutes from "./routes/user.routes.js";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./config/database.config.js";
+import advantageRoutes from "./routes/advantage.routes.js";
 import blogRoutes from "./routes/blog.routes.js";
+import commentRoutes from "./routes/comment.routes.js";
+import dashboardRoutes from "./routes/dashboard.routes.js";
+import educationRoutes from "./routes/education.routes.js";
+import experienceRoutes from "./routes/experience.routes.js";
+import fileRoutes from "./routes/file.routes.js";
+import portfolioRoutes from "./routes/portfolio.routes.js";
+import serviceRoutes from "./routes/service.routes.js";
+import testimonialRoutes from "./routes/testimonial.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
+const port = process.env.PORT;
 const app = express();
-const PORT = process.env.PORT;
 
-/* Middleware */
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
-/* Test Route */
-app.get("/", (req, res) => {
-  res.send("🚀 Server is running successfully!");
-});
-
-/* DB */
 connectDB();
 
-app.use("api/v1/auth", userRoutes);
-app.use("api/v1/blogs", blogRoutes);
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/blogs", blogRoutes);
+app.use("/api/v1/advantages", advantageRoutes);
+app.use("/api/v1/testimonials", testimonialRoutes);
+app.use("/api/v1/comments", commentRoutes);
+app.use("/api/v1/files", fileRoutes);
+app.use("/api/v1/experiences", experienceRoutes);
+app.use("/api/v1/portfolios", portfolioRoutes);
+app.use("/api/v1/services", serviceRoutes);
+app.use("/api/v1/education", educationRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
-/* Server */
-app.listen(PORT, () => {
-  console.log(`🔥 Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Portfolio Backend Server is Running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
